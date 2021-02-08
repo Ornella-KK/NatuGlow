a3490bbb786d7a5af98ef7a32cf2c5de9459d596
Ornella-KK <ornellakankazi@gmail.com>
2021-02-08 13:45:13 +0200

    made models and create my first view with an index html template

--- glow/models.py
+++ glow/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from tinymce.models import HTMLField
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Category (models.Model):
+    title = models.CharField(max_length=20)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    
+class Routine (models.Model):
+    name = models.CharField(max_length=20,blank=True)
+    products = HTMLField(max_length=500,default='Products to use')
+    description = HTMLField(max_length=500,default='How to use')
+    category = models.ForeignKey(Category,on_delete=models.CASCADE) 
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    @classmethod
+    def search_by_routine(cls,search_term):
+        routine = cls.objects.filter(name__icontains=search_term)
+        print(routine)
+        return routine
+    
+    def __str__(self):
+        return self.name
+
+class Skintype (models.Model):
+    skin = models.CharField(max_length=20,blank=False)
+    details = HTMLField(max_length=500,default='How to know')
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
--- glow/views.py
+++ glow/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http import Http404
+from .models import *
+from django.contrib.auth.models import User
 # Create your views here.
+def home(request):
+    routine = Routine.objects.all()
+    
+    context = {
+        'routine':routine,
+    }
+    return render(request,'index.html',context)
